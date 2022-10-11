a0ebb6fd171ee193d8a14c572ca3fc61c92f591a
Hakan Bilgi <hknblg34@gmail.com>
2022-10-11 18:31:40 +0300

    data js

---
+++ src/assest/data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: 1,
+    name: "Helen Blood",
+    job: "Grand Mother",
+    comment: "Our living values...",
+    img: "https://images.pexels.com/photos/2050994/pexels-photo-2050994.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  },
+  {
+    id: 2,
+    name: "Jesica White",
+    job: "Auntie",
+    comment: "Living beauties...",
+    img: "https://images.pexels.com/photos/2050979/pexels-photo-2050979.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  },
+  {
+    id: 3,
+    name: "Adam Mahogany",
+    job: "Grand Fatherher",
+    comment: "Our living treasures.",
+    img: "https://images.pexels.com/photos/35011/people-peoples-homeless-male.jpg?auto=compress&cs=tinysrgb&w=1600",
+  },
+  {
+    id: 4,
+    name: "Jason Brown",
+    job: "Uncle",
+    comment: "Living monuments..",
+    img: "https://images.pexels.com/photos/35183/people-homeless-man-male.jpg?auto=compress&cs=tinysrgb&w=1600",
+  },
+];
+
+export default data;
